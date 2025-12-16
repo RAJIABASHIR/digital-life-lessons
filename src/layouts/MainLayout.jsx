83212f2b06a8231b50224e0400fa7effb2e4f8cc
Rajia Bashir Rahi <rajiabashirrahi@Rajias-MacBook-Air.local>
2025-12-15 23:36:48 -0500

    add main layout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  const location = useLocation();
+  const hideLayout = location.pathname === "/404"; // or use wildcard
+
+  if (hideLayout) return <Outlet />;
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
